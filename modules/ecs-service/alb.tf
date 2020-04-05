38677d1b2c9bf063c272f0c1025142c5b570e459
Anthony Nash <anthony.nash@du.co>
2020-04-05 10:37:45 +0100

    Allow for multiple ports in alb

--- modules/ecs-service/alb.tf
+++ modules/ecs-service/alb.tf
@@ -2,27 +2,31 @@
 # target
 #
 resource "aws_alb_target_group" "ecs-service" {
-  name = "${var.application_name}-${substr(
-    md5(
-      format(
-        "%s%s%s",
-        var.application_port,
-        var.deregistration_delay,
-        var.healthcheck_matcher,
+  for_each = zipmap(formatlist("%s-%s", local.lb_ports.*.container, local.lb_ports.*.port), local.lb_ports)
+  name = format("%s-%s",
+    each.key,
+    substr(
+      md5(
+        format(
+          "%s%s%s",
+          each.value.port,
+          var.deregistration_delay,
+          var.healthcheck_matcher,
+        )
       ),
-    ),
-    0,
-    12,
-  )}"
-  port                 = var.application_port
-  protocol             = "HTTP"
+      0,
+      12,
+    )
+  )
+  port                 = each.value.port
+  protocol             = each.value.port == 443 ? "HTTPS" : "HTTP"
   vpc_id               = var.vpc_id
   deregistration_delay = var.deregistration_delay
 
   health_check {
     healthy_threshold   = 3
     unhealthy_threshold = 3
-    protocol            = "HTTP"
+    protocol            = each.value.port == 443 ? "HTTPS" : "HTTP"
     path                = "/"
     interval            = 60
     matcher             = var.healthcheck_matcher
